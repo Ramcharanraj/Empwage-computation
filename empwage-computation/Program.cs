fa18b951e733cef24eec334c475890be5df2bd9b
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-09-29 11:33:03 +0530

    ADDED: Calculation of Length

--- empwage-computation/Program.cs
+++ empwage-computation/Program.cs
@@ -7,6 +7,9 @@ namespace empwage_computation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Empwage-computation program");
+            
+            LengthOfLines length = new LengthOfLines(4, 6, 8, 10);
+            Console.WriteLine(length);
         }
     }
 }
