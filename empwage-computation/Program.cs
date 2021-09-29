e1a2c23d066432028455f8c5b563962d94828a83
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-09-29 10:57:53 +0530

    ADDED: Welcome Message

---
+++ empwage-computation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace empwage_computation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Empwage-computation program");
+        }
+    }
+}
